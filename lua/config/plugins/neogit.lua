8ac067e9b1d0654f461810674ef88b295002c183
Alejandro Gonzalez <alex.glez.felix@gmail.com>
2025-03-05 23:21:30 -0600

    git new integrations

--- lua/config/plugins/neogit.lua
+++ lua/config/plugins/neogit.lua
@@ -1,32 +1,38 @@
 return {
-  'NeogitOrg/neogit',
-  dependencies = {
-	'nvim-lua/plenary.nvim', -- required
-	'sindrets/diffview.nvim', -- optional - Diff integration
+	"NeogitOrg/neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"sindrets/diffview.nvim", -- optional - Diff integration
 
-	-- Only one of these is needed, not both.
-	'nvim-telescope/telescope.nvim', -- optional
-	'ibhagwan/fzf-lua', -- optional
-  },
-  config = function()
-	local neogit = require 'neogit'
-	neogit.setup {}
+		-- Only one of these is needed, not both.
+		"nvim-telescope/telescope.nvim", -- optional
+		"ibhagwan/fzf-lua", -- optional
+	},
+	config = function()
+		local neogit = require("neogit")
+		neogit.setup({
+			graph_style = "unicode",
+			integrations = {
+				telescope = true,
+				diffview = true,
+				fzf = true,
+			},
+		})
 
-	vim.keymap.set('n', '<leader>gn', function()
-	  neogit.open()
-	end, { desc = '[g]it [n]eo' })
-	vim.keymap.set('n', '<leader>gc', function()
-	  neogit.open { 'commit' }
-	end, { desc = '[g]it [c]commit' })
-	vim.keymap.set('n', '<leader>gP', function()
-	  neogit.open { 'push' }
-	end, { desc = '[g]it [P]ush' })
-	vim.keymap.set('n', '<leader>gp', function()
-	  neogit.open { 'pull' }
-	end, { desc = '[g]it [p]ull' })
-	vim.keymap.set('n', '<leader>gs', function()
-	  neogit.open { 'stash' }
-	end, { desc = '[g]it [s]stash' })
-  end,
+		vim.keymap.set("n", "<leader>gn", function()
+			neogit.open()
+		end, { desc = "[g]it [n]eo" })
+		vim.keymap.set("n", "<leader>gc", function()
+			neogit.open({ "commit" })
+		end, { desc = "[g]it [c]commit" })
+		vim.keymap.set("n", "<leader>gP", function()
+			neogit.open({ "push" })
+		end, { desc = "[g]it [P]ush" })
+		vim.keymap.set("n", "<leader>gp", function()
+			neogit.open({ "pull" })
+		end, { desc = "[g]it [p]ull" })
+		vim.keymap.set("n", "<leader>gs", function()
+			neogit.open({ "stash" })
+		end, { desc = "[g]it [s]stash" })
+	end,
 }
-
